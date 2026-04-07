3e9ce7ee30365e4de4d7bd042ad8db91e8533d99
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2026-04-07 04:23:02 -0300

    Add: adding route params for home screen

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,16 +3,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Login } from "../screens/login";
 import { Register } from "../screens/register";
+import { Home } from "../screens/home";
+import { RootStackParamList } from "../screens/types/screen_types";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export function AppRoutes(){
 
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="register" screenOptions={{headerShown: false}} >
+            <Stack.Navigator initialRouteName="home" screenOptions={{headerShown: false}} >
                 <Stack.Screen name="login" component={Login} />
                 <Stack.Screen name="register" component={Register}/>
+                <Stack.Screen name="home" component={Home} initialParams={{role: "STUDENT"}}/>
             </Stack.Navigator>
 
         </NavigationContainer>
