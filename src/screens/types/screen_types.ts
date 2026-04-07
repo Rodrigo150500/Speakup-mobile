567b1ac301e53cf23a1eca55a6e2db5b8d52735b
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2026-04-07 04:22:24 -0300

    add: adding param for home screen

--- src/screens/types/screen_types.ts
+++ src/screens/types/screen_types.ts
@@ -1,5 +1,9 @@
+import { RouteProp } from "@react-navigation/native"
+
 export type RootStackParamList = {
-    home: undefined,
+    home: {role: "TEACHER" | "STUDENT"},
     login: undefined,
     register: undefined
 }
+
+export type HomeRouteProp = RouteProp<RootStackParamList, "home">
